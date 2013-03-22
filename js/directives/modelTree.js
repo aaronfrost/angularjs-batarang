cbe21e84edc73bf6ad52da8a293cee1c7157f991
Brian Ford <btford@umich.edu>
2013-03-22 06:16:02 -0700

    wip

--- js/directives/modelTree.js
+++ js/directives/modelTree.js
@@ -4,33 +4,39 @@ angular.module('panelApp').directive('batModelTree', function ($compile) {
   var modelState = {};
   var scopeState = {};
 
+  var selected = null;
+
+  var template = 
+    '<div class="scope-branch">' +
+      '<a href ng-click="inspect()">&lt;</a>' +
+      '<a href ng-click="select(val.id)">Scope ({{val.id}})</a>' +
+      '<div ng-repeat="child in val.children">' +
+        '<bat-model-tree ' +
+          'val="child" ' +
+          'inspect="inspect" ' +
+          'selected="selected">' +
+        '</bat-model-tree>' +
+      '</div>' +
+    '</div>';
+
   return {
     restrict: 'E',
     terminal: true,
     scope: {
       val: '=',
-      edit: '=',
+      select: '=',
+      selected: '=',
       inspect: '='
     },
     link: function (scope, element, attrs) {
       // this is more complicated then it should be
       // see: https://github.com/angular/angular.js/issues/898
-      element.append(
-        '<div class="scope-branch">' +
-          '<a href ng-click="inspect()">Scope ({{val.id}})</a>' +
-          '<span ng-show="val.children.length"> | <a href ng-click="scopeState[val.id] = !scopeState[val.id]">scopes</a></span>' +
-
-          '<div ng-hide="scopeState[val.id]">' +
-            '<div ng-repeat="child in val.children">' +
-              '<bat-model-tree val="child" inspect="inspect" edit="edit"></bat-model-tree>' +
-            '</div>' +
-          '</div>' +
-
-        '</div>');
+      element.append(template);
 
       var childScope = scope.$new();
-      childScope.modelState = modelState;
-      childScope.scopeState = scopeState;
+      childScope.select = scope.select = function (id) {
+        selected = id;
+      };
 
       $compile(element.contents())(childScope);
     }
